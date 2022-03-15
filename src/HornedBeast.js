dfe0b15f6d7077cd04b8617a631d77893185bea1
Roger Wells <rsw359@gmail.com>
2022-03-16 07:36:55 +0900

    added event listener

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -4,15 +4,26 @@ class HornedBeast extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      votes : 0
+      favorites : 0
     }
   }
+
+  handleFavs = () =>{
+    this.setState({
+      favorites : this.state.favorites + 1
+    })
+  }
+
   render() {
     return (
     <div>
       <h2>{this.props.title}</h2>
-      <img src = {this.props.imgUrl} alt = ""/>
+      <img src = {this.props.imgUrl} alt = "" onClick={this.handleFavs} />
+      <p>
+        {this.state.favorites} Favorites
+      </p>
       <p>{this.props.description}</p>
+
     </div>
     )
   }
