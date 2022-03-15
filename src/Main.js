1b2603c8973837c02bfa93cfd9c726bf56b5e565
Roger Wells <rsw359@gmail.com>
2022-03-15 11:08:11 +0900

    completed secont component

--- src/Main.js
+++ src/Main.js
@@ -10,6 +10,12 @@ class Main extends React.Component {
       imgUrl = "https://media.savetherhino.org/prod/uploads/2018/02/Species-Black-Credit-Phill-Perry-768x511.jpg" 
       description = "This is a horned beast"
       />
+
+      <HornedBeast 
+      title = "Triceratos" 
+      imgUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN4j1_TxHu22qjrXzibu-Xpi9RYX9giM0fpQ&usqp=CAU"
+      description = "This too is a horned beast"
+      />
       
 
       </main>
