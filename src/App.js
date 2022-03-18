6da641dd6e095b1c2f512d588cdf7dfc52be3baa
Roger Wells <rsw359@gmail.com>
2022-03-18 10:04:03 +0900

    implemented form

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Header from "./Header.js";
 import Main from './Main';
 import Footer from './Footer';
 import SelectedBeast from './SelectedBeast'
-import data from './data.json'
-
+import data from './data.json';
+import { Form } from 'react-bootstrap';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedBeast: {},
-      showModal:false,     
-      
+      showModal: false,
+      listBeast: data,
     }
-   
+
   };
 
 
@@ -24,18 +24,18 @@ class App extends React.Component {
     console.log('before', this.state)
     this.setState({
       showModal: true,
-    
-    
+
+
     });
     console.log('after', this.state)
   };
- 
+
   hideModal = () => {
     this.setState({
       showModal: false,
     });
   };
-  
+
   stateFunction = (title) => {
     console.log(title);
     let thisBeast = data.find(beast => beast.title === title)
@@ -45,20 +45,45 @@ class App extends React.Component {
     console.log(this.state.selectedBeast);
   }
 
+  handleSelect = (e) => {
+    let selected = parseInt(e.target.value)
+    console.log(e.target.value)
+    let beasties = data.filter(hornNum => hornNum.horns === selected);
+    console.log(beasties);
+    this.setState({listBeast: beasties});   
+
+  };
+  
+
   render() {
+    console.log(this.state.listBeast);
     return (
       <>
-        <Header className="headerCont" />
+        <Header className="headerCont"/>
+
+        <Form>
+
+          <Form.Select name="selected" onChange={(this.handleSelect)}>
+            <option>Select number of Horns</option>
+            <option value="1">One Horn</option>
+            <option value="2">Two Horns</option>
+            <option value="3">Three Horns</option>
+            <option value="100">Super Horned!</option>
+          </Form.Select>
+
+        </Form>
+
         <Main className="MainCont"
           data={data}
           stateFunction={this.stateFunction}
-          handleModal = {this.handleModal}
-          
+          handleModal={this.handleModal}
+          listBeast = {this.state.listBeast}
+
         />
-        <SelectedBeast 
-        theBeast = {this.state.selectedBeast}
-        showModal = {this.state.showModal}
-        hideModal = {this.hideModal}
+        <SelectedBeast
+          theBeast={this.state.selectedBeast}
+          showModal={this.state.showModal}
+          hideModal={this.hideModal}
         />
         <Footer className="FootCont" />
 
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import './Header.css'
+
+import './Header.css';
 
 class Header extends React.Component {
+  
   render() {
     return (
-    <header>
-      <h1>Beasts of Horns</h1>
-      <p>This a collection of things with horns. Click on your favorite!</p>
-    </header>
+      <header>
+        <h1>Beasts of Horns</h1>
+        <p>This a collection of things with horns. Click on your favorite!</p>
+      </header>
     )
   }
 }
